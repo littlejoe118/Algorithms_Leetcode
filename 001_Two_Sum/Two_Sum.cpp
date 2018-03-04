5a73ce546719341257e9c054275956eada34b940
Rookie118 <chaowei39miku@163.com>
2018-03-04 15:44:02 +0800

    Update and rename 001_Two_Sum.cpp to Two_Sum.cpp

--- 001_Two_Sum/Two_Sum.cpp
+++ 001_Two_Sum/Two_Sum.cpp
@@ -2,7 +2,6 @@ class Solution {
 public:
     vector<int> twoSum(vector<int>& nums, int target) {
         vector<int> indices;
-        //int count = 1;
         for(int i = 0; i < nums.size() - 1; ++i)
         {
             for(int j = i + 1; j < nums.size(); ++j)
@@ -14,7 +13,6 @@ public:
                     return indices;
                 }
             }
-            //++count;
         }
         if(indices.size() == 0)
             return indices;
