8643f4032761cb44f3f927468a0070c498d92fe9
Rookie118 <chaowei39miku@163.com>
2018-03-09 21:42:58 +0800

    Update 013_Roman_to_Integer.cpp

--- 013_Roman_to_Integer/013_Roman_to_Integer.cpp
+++ 013_Roman_to_Integer/013_Roman_to_Integer.cpp
@@ -8,28 +8,28 @@
 class Solution {
 public:
     int romanToInt(string s) {
-        if(s.size() == 0)
+        if(s.size() == 0)  //s长度为0，则返回0
             return 0;
         
         int sum = 0;
-        for(int i = 0; i < s.size(); ++i)
+        for(int i = 0; i < s.size(); ++i)  
         {
             switch(s[i])
             {
                 case 'I':
-                    if(s[i + 1] == 'V' || s[i + 1] == 'X')
+                    if(s[i + 1] == 'V' || s[i + 1] == 'X')  //I的下一个字母为V或者X，则代表减1
                         sum -= 1;
                     else
                         sum += 1;
                     break;
                 case 'X':
-                    if(s[i + 1] == 'L' || s[i + 1] == 'C')
+                    if(s[i + 1] == 'L' || s[i + 1] == 'C')  //X的下一个字母为L或者C，则代表减10
                         sum -= 10;
                     else
                         sum += 10;
                     break;
                 case 'C':
-                    if(s[i + 1] == 'D' || s[i + 1] == 'M')
+                    if(s[i + 1] == 'D' || s[i + 1] == 'M')  //C的下一个字母为D或者M，则代表减100
                         sum -= 100;
                     else
                         sum += 100;
