ddbce143aa130ab68a36de469323c43761d6076f
Rookie118 <chaowei39miku@163.com>
2018-03-09 21:30:56 +0800

    Update 005_Longest_Palindromic_Substring.cpp

--- 005_Longest_Palindromic_Substring/005_Longest_Palindromic_Substring.cpp
+++ 005_Longest_Palindromic_Substring/005_Longest_Palindromic_Substring.cpp
@@ -2,14 +2,16 @@
 //Title: Longest Palindromic Substring
 //Completion time: 2018-03-09
 
-/*************The first version*************/
+/**********The first version****************/
+/**********Time complexity: O(n^3)**********/
+/**********Space complexity: O(n)*********/
 class Solution {
 public:
-    bool isPalindromic(string s, int length)
+    bool isPalindromic(string s, int length)  //判断字符串s是不是回文
     {
-        for(int i = 0; i < length / 2; ++i)
+        for(int i = 0; i < length / 2; ++i)  //从字符串的前半段开始循环
         {
-            if(s[i] != s[length - i - 1])
+            if(s[i] != s[length - i - 1])  //如果两者不想等说明s不是回文
                 return false;
         }
         return true;
@@ -19,26 +21,25 @@ public:
         string sub, max_str;
         int max_len = 0;
         
-        if(s.size() < 2)
+        if(s.size() < 2)  //s长度小于2，直接返回s
             return s;
         
-        for(int i = 0; i < s.size(); ++i)
-            for(int j = s.size() - 1; j >= i; --j)
+        for(int i = 0; i < s.size(); ++i)  //从s第一个字符找到最后一个字符
+            for(int j = s.size() - 1; j >= i; --j)  //从右往左遍历，直到找到s[i]等于s[j]
             {
-                if(s[i] == s[j])
+                if(s[i] == s[j])  //说明第i个字符到第j个字符可能是回文子串
                 {
-                    sub = s.substr(i, j - i + 1);
-                    if(isPalindromic(sub, j - i + 1))
+                    sub = s.substr(i, j - i + 1);  //提取子串
+                    if(isPalindromic(sub, j - i + 1))  //判断是否为回文子串
                     {
-                        if(sub.size() == s.size())
+                        if(sub.size() == s.size())  //如果第一个子串长度和s长度相等，则直接返回s
                             return s;
-                        if(max_len < j - i + 1)
+                        if(max_len < j - i + 1)  //max_len小于子串长度，则max_len重新赋值
                         {
                             max_len = j - i + 1;
-                            max_str = sub;
+                            max_str = sub;  //max_str重新赋值
                         }   
-                    }
-                        
+                    }        
                 }
             }
         return max_str;
