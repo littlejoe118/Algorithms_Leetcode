ee48ffed7495400ed5229a254006db4c15ab3011
Rookie118 <chaowei39miku@163.com>
2018-03-18 19:20:44 +0800

    Update Two_Sum.cpp

--- 001_Two_Sum/Two_Sum.cpp
+++ 001_Two_Sum/Two_Sum.cpp
@@ -11,9 +11,9 @@ class Solution {
 public:
     vector<int> twoSum(vector<int>& nums, int target) {
         vector<int> indices;
-        for(int i = 0; i < nums.size() - 1; ++i)
+        for(int i = 0; i < nums.size() - 1; ++i)  //遍历前nums.size() - 1个数
         {
-            for(int j = i + 1; j < nums.size(); ++j)
+            for(int j = i + 1; j < nums.size(); ++j)  //遍历后nums.size() - i - 1个数
             {
                 if(nums[i] + nums[j] == target)
                 {
@@ -25,5 +25,34 @@ public:
         }
         if(indices.size() == 0)
             return indices;
+    }
+};
+
+/*
+ *Author: Rookie118
+ *Title: Two Sum
+ *Completion time: 2018-03-18
+*/
+
+//The second version
+//Time complexity: O(n)
+//Space complexity: O(1)
+class Solution {
+public:
+    vector<int> twoSum(vector<int>& nums, int target) {
+        unordered_map<int, int> mp;  //存储nums值和对应的序号的键值对
+        vector<int> result;
+        
+        for(int i = 0; i < nums.size(); ++i) 
+        {
+            if(mp.find(target - nums[i]) != mp.end())  //找到另外一个加数，则存储两个加数的对应序号
+            {
+                result.push_back(mp[target - nums[i]]);
+                result.push_back(i);
+                break;
+            }
+            mp[nums[i]] = i;  //对mp进行初始化
+        }
+        return result;
     }
 };
