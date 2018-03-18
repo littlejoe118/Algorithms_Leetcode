dfecb20e6e39c81a4033420a0c1dfdeced0972eb
Rookie118 <chaowei39miku@163.com>
2018-03-18 17:19:33 +0800

    Update 020_Valid_Parentheses.cpp

--- 020_Valid_Parentheses/020_Valid_Parentheses.cpp
+++ 020_Valid_Parentheses/020_Valid_Parentheses.cpp
@@ -10,27 +10,27 @@
 class Solution {
 public:
     bool isValid(string s) {
-        vector<char> stack;
-        char record;
+        vector<char> stack;  //模拟栈
+        char record;  //记录栈顶退出的元素
         
-        if(s.length() < 2)
+        if(s.length() < 2)  //排除无效输入
             return false;
 
-        for(int i = s.length()-1; i >= 0; --i)
+        for(int i = s.length()-1; i >= 0; --i)  //从右往左遍历
         {
-            if(s[i] != '(' && s[i] != '[' && s[i] != '{')
+            if(s[i] != '(' && s[i] != '[' && s[i] != '{')  //是]，}，) 符号则压入栈中
                 stack.push_back(s[i]);
-            if(s[i] == '(' || s[i] == '[' || s[i] == '{')
+            if(s[i] == '(' || s[i] == '[' || s[i] == '{')  //是(，[，{符号则弹出栈
             {
-                if(stack.size() == 0)
+                if(stack.size() == 0)  //栈中没有元素则表明是无效输入
                     return false;
                 record = stack.back();
                 stack.pop_back();
                 if((s[i] == '(' && record != ')') || (s[i] == '[' && record != ']') || (s[i] == '{' && record != '}'))
-                    return false;       
+                    return false;  //括号不匹配则表明是无效输入
             }   
         }
-        if(stack.size() != 0)
+        if(stack.size() != 0)  //栈中还有元素则表明是无效输入
             return false;
         else
             return true; 
